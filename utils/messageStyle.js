958f4090d0ba5277d1bb59fc58117ce20dd397a4
hassan0103948563-afk <hassan0103948563@gmail.com>
2026-04-19 01:36:23 +0000

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Digix Crew",
+        body: "•⸙𓍝ϻ𝆭ℜ 𝐃ɛ፝֟֯ؖ۬𝐯ɪs ᑭℜɪϻ𝆭ɛ 𓃵ᭃ𓍝주님 𓍝",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
